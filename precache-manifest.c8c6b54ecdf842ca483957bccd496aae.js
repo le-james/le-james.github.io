83d430c1a71380c054f1a3ef2c0f28bbd735b1f5
James Le <le_james@outlook.com>
2022-03-20 20:45:30 -0700

    Updates

---
+++ precache-manifest.c8c6b54ecdf842ca483957bccd496aae.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fa23c0aab230b6213448ac74f965ed91",
+    "url": "/index.html"
+  },
+  {
+    "revision": "41c8dfa96be656a4984f",
+    "url": "/static/css/main.54c9968c.chunk.css"
+  },
+  {
+    "revision": "b76d1c8f8131b0883bc1",
+    "url": "/static/js/2.12092b1d.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.12092b1d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "41c8dfa96be656a4984f",
+    "url": "/static/js/main.bad3379a.chunk.js"
+  },
+  {
+    "revision": "8da32427f6dff9b5abfa",
+    "url": "/static/js/runtime-main.99e80802.js"
+  }
+]);
