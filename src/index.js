fd684e168a4c618ef3b04ea134ad19e59c659e19
James Le <le_james@outlook.com>
2022-08-03 22:20:30 -0700

    react router refresh fix

--- src/index.js
+++ src/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import { BrowserRouter as Router } from "react-router-dom";
+import { HashRouter as Router } from "react-router-dom";
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename='/'>
+    <Router>
       <App />
     </Router>
   </React.StrictMode>,
