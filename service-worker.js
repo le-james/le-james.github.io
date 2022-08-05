2e0484ab64b9f7dd890b2e85bb151123d3f3bb53
James Le <le_james@outlook.com>
2022-08-05 09:21:32 -0700

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.364e9bad0db587dcb087801876e1d799.js"
+  "/precache-manifest.cc79eac20a71e5f0874ea4fba17d3c30.js"
 );
 
 self.addEventListener('message', (event) => {
